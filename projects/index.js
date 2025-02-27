125380aa1da18eab2febf4864dd0018bb1bfe411
Danny Shinjiro Inoue <dan023@ucsd.edu>
2025-02-26 16:28:54 -0800

    Lab 4

--- projects/index.js
+++ projects/index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects } from '../global.js';
 
 async function displayLatestProjects() {
     const projects = await fetchJSON('./lib/projects.json');
