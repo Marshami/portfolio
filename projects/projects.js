d5528e705bd736d8bc924d1caef9c6c19e197e22
Danny Shinjiro Inoue <dan023@ucsd.edu>
2025-02-26 17:00:36 -0800

    Lab 4

--- projects/projects.js
+++ projects/projects.js
@@ -12,9 +12,14 @@ export function renderProjects(projects, containerElement) {
     });
 }
 
+import { fetchJSON, renderProjects } from '../global.js';
+
 async function loadProjects() {
-    const projects = await fetchJSON('../lib/projects.json');
-    const projectsContainer = document.querySelector('.projects');
-    document.querySelector('.projects-title').textContent = `Projects (${projects.length})`;
-    renderProjects(projects, projectsContainer, 'h2');
+    const projectsContainer = document.querySelector('.projects'); // Select the container
+    if (!projectsContainer) return; // Stop if the container is missing
+
+    const projects = await fetchJSON('../lib/projects.json'); // Fetch projects
+    renderProjects(projects, projectsContainer, 'h2'); // Render projects
 }
+
+loadProjects();
